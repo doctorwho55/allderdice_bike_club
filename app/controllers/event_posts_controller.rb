b27144e1dbbff7c1b3fdb11e7933f9866354d046
Prelang Builder <builder@prelang.com>
2016-05-18 20:14:52 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/event_posts_controller.rb
+++ app/controllers/event_posts_controller.rb
@@ -1,4 +1,8 @@
 class EventPostsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_event_post, only: [:show, :edit, :update, :destroy]
 
   # GET /event_posts
